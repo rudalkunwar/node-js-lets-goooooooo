f3cb1a0c323d5cc9dd8df771fa6f943c5a3067f5
Rudal Kunwar <ruzalkunwar@gmail.com>
2024-01-20 16:07:02 +0545

    complete

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -44,7 +44,7 @@ const blog_edit = (req, res) => {
       res.render("blogs/edit", { blog: data });
     })
     .catch((err) => {
-      console.log(err);
+      res.status(404).render("404");
     });
 };
 const blog_update = (req, res) => {
@@ -65,7 +65,7 @@ const blog_delete = (req, res) => {
       res.json({ redirect: "/blog" });
     })
     .catch((err) => {
-      console.log(err);
+      res.status(404).render("404");
     });
 };
 
